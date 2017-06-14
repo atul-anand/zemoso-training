66508558662bf22a687bef9ef4dfd2ac7a4091a1
Atul Anand <atul.anand@zemosolabs.com>
2017-06-14 06:39:52 +0000

    assgn2

--- java/atul/assignment2/OverloadConstruct.java
+++ java/atul/assignment2/OverloadConstruct.java
@@ -1,3 +1,17 @@
 //: atul/assignment2/OverloadConstruct.java
 
 package atul.assignment2.overloadconstruct;
+
+public class OverloadConstruct {
+
+	OverloadConstruct(){
+		System.out.println("first");
+	}
+	OverloadConstruct(int a){
+		this();
+		System.out.println("second");
+	}
+	public static void main(String[] args){
+		OverloadConstruct over = new OverloadConstruct(12);
+	}
+}
