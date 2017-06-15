ce8540011f7806d0b3ce288c65e2b707a8db00ad
Atul Anand <atul.anand@zemosolabs.com>
2017-06-15 10:01:19 +0000

    Java Assignment 1

---
+++ java/atul/jassignment1/FileRead.java
@@ -0,0 +1,35 @@
+//: atul/jassignment1/FileRead.java
+
+package atul.jassignment1.fileread;
+
+import java.io.*;
+import java.util.*;
+import java.util.regex.*;
+public class FileRead {
+	static void matchRegExp(String filePath, String expression){
+		Pattern pattern = Pattern.compile(expression);
+                Matcher matcher = pattern.matcher(filePath);
+                if(matcher.find())
+                        System.out.println(filePath);
+	}
+	static void listFilesDirectory(File dir, String expression){
+		try {
+			File[] files = dir.listFiles();
+			for(File file:files){
+				if(file.isDirectory())
+					listFilesDirectory(file,expression);
+				matchRegExp(file.getCanonicalPath().toString(), expression);
+			}
+		} catch (IOException e) {
+		}
+	}
+	public static void main(String[] args){
+		Scanner scanner = new Scanner(System.in);
+		while(scanner.hasNext()){
+			String expression = scanner.nextLine();
+			File[] files = new File(".").listFiles();
+			for(File f:files)
+				listFilesDirectory(f,expression);
+		}
+	}
+}
