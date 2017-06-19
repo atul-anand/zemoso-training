0cc310d453256cbbc469fac8faf22d5782995e77
Atul Anand <atul.anand@zemosolabs.com>
2017-06-19 07:44:01 +0000

    JavaAssignmentPingHost

--- java/atul/javaAssignments/PingHost.java
+++ java/atul/javaAssignments/PingHost.java
@@ -3,40 +3,54 @@
 import java.net.*;
 import java.util.*;
 public class PingHost {
+	static Long currentTime(){
+		return System.currentTimeMillis();
+	}
 	static Long medianTime(ArrayList<Long> times){
 		Collections.sort(times, Collections.reverseOrder());
 		if(times.size()%2==0)
 			return (times.get(times.size()/2)+times.get(times.size()/2-1))/2;
 		return times.get(times.size()/2);
 	}
-	static Long pingMedianTime(String ipAddress, int numberOfPings) throws Exception{
-		InetAddress inet = InetAddress.getByName(ipAddress);
+	static Long pingMedianTime(String ipAddress, int ipPort, int numberOfPings) throws Exception{
                 ArrayList<Long> times = new ArrayList<Long>();
-		Calendar cal = new GregorianCalendar();
-                Long finish = cal.getTimeInMillis();
-                Long start = cal.getTimeInMillis();
-		System.out.println(numberOfPings + "  " + start);
-		while(numberOfPings>0){
+                Long finish = currentTime();
+                Long start = currentTime();
+		for(int i=0;i<numberOfPings;i++){
 			try {
-				start = cal.getTimeInMillis();
-			        if(inet.isReachable(5000)) {
-	        	                finish = cal.getTimeInMillis();
-        	        	        times.add(finish-start);
-		                }
+				Socket socket = new Socket();
+		                InetAddress inet = InetAddress.getByName(ipAddress);
+        	        	SocketAddress remoteSocketAddress = new InetSocketAddress(inet, ipPort);
+				start = currentTime();
+				socket.connect(remoteSocketAddress,1000);
+	       	                finish = currentTime();
+				socket.close();
+                	        times.add(finish-start);
+				Long pingTime = finish - start;
+				System.out.println("Ping Successful@" + inet + " " + start + "ms " + finish + "ms " + pingTime + "ms");
 			} catch (Exception e){
 				System.out.println(e.getMessage());
 			}
-			System.out.println(inet + " " + start + " " + finish + " " + numberOfPings);
-			numberOfPings--;
-		}
-		System.out.println(numberOfPings);
-		if(times.size()!=0)
+		}		if(times.size()!=0)
 			return medianTime(times);
 		return new Long(0);
 	}
 	public static void main(String[] args) throws Exception{
-		String ipAddress = "192.168.0.1";
-		int numberOfPings = 10;
-		System.out.println(pingMedianTime(ipAddress,numberOfPings));
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter Address: ");
+		String ipAddress = scanner.nextLine();
+		System.out.print("Enter Port: ");
+		int ipPort = scanner.nextInt();
+		System.out.print("Enter No of Pings: ");
+		int numberOfPings = scanner.nextInt();
+		System.out.println("Median Ping Time: " + pingMedianTime(ipAddress,ipPort,numberOfPings)+"ms");
 	}
 }
+
+
+
+
+
+
+
+
