3c9573dc551ad99fdc9db610ad3257abd9dfbe7f
Atul Anand <atul.anand@zemosolabs.com>
2017-07-02 16:26:11 +0530

    UML

--- design/XEIndia/CurrencyChecker.java
+++ design/XEIndia/CurrencyChecker.java
@@ -3,12 +3,12 @@ public class CurrencyChecker implements Observer {
 	private double inr;
 	private static int observersCount = 0;
 	private final int observerID=++observersCount;
-	private Subject currencyConvertor;
-	public CurrencyChecker(Subject currencyConvertor, double inr){
-		this.currencyConvertor = currencyConvertor;
+	private Subject convertor;
+	public CurrencyChecker(Subject convertor, double inr){
+		this.convertor = convertor;
 		this.inr = inr;
 		System.out.println("New observer "+this.observerID);
-		currencyConvertor.add(this);
+		convertor.add(this);
 	}
 	public void update(){
 		printRates();
@@ -20,8 +20,8 @@ public class CurrencyChecker implements Observer {
 	public void printRates(){
 		DecimalFormat df = new DecimalFormat("#.00");
 		System.out.println("Observer " + observerID + " INR:" + inr + 
-							"\nUSD:" + df.format(inr/(currencyConvertor.getUSD())) +
-							"\nGBP:" + df.format(inr/(currencyConvertor.getGBP())) + 
-							"\nEuro:" + df.format(inr/(currencyConvertor.getEuro())) + "\n");
+							"\nUSD:" + df.format(inr/(convertor.getUSD())) +
+							"\nGBP:" + df.format(inr/(convertor.getGBP())) + 
+							"\nEuro:" + df.format(inr/(convertor.getEuro())) + "\n");
 	}
 }
